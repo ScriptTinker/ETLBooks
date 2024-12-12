7b327c8956f411e4557172cda73f98b8f7f21d58
IcantCode:) <moraruedi11@outlook.com>
2024-12-12 21:34:42 +0100

    Code clean up and preparation

--- ETLBooks_flask/__init__.py
+++ ETLBooks_flask/__init__.py
@@ -2,6 +2,7 @@ from flask_bcrypt import Bcrypt
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+import os
 
 app = Flask(__name__)
 
@@ -9,7 +10,7 @@ bycrypt = Bcrypt(app)
 
 login_manager = LoginManager(app)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///ETLbook.db"
 
 app.config ["SECRET_KEY"] = ""
 
@@ -18,3 +19,9 @@ db = SQLAlchemy(app)
 login_manager.login_view = "login"
 
 login_manager.login_message_category = "info"
+
+from ETLBooks_flask.models import User,Book
+with app.app_context():
+    if not os.path.exists('/istance/ETLbook.db'):
+        db.create_all()
+        
